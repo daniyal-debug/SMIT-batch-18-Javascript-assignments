97e41156facba3d8452e7867bed5e9bee7c7ee1b
daniyal-debug <daniyalsuleman61@gmail.com>
2025-10-11 00:00:30 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,28 @@
+// Chapter 1
+
+alert("Welcome to JavaScript World!");
+alert("Hello again! \n How’s your coding journey?");
+alert("JavaScript says: Let's make the web alive!");
+
+// Chapter 2
+
+alert("My name is Daniyal!");
+alert("My Age Is 18!");
+alert("Learning web Dev HTML, CSS, and JS!");
+
+alert("Coding\nCodin\nCodi\nCod\nCo\nC");
+
+alert("Stay connected at: daniyalsuleman61@gmail.com");
+
+alert("Smart work beats hard work every time!");
+
+alert("Yes! This is JavaScript!");
+
+alert("Fun fact: I started coding before breakfast!");
+
+// Chapter 3
+
+alert("I Have Started Coding!");
+
+alert("I was born to code — somewhere around 2007!");
+alert("Daniyal just ordered 2 pair of amazing Shoes from EcomStore!");
