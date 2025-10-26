4aef507fce7c1a7a17a6f43faa77fa02c13c1225
Muhammad Daniyal <daniyalsuleman61@gmail.com>
2025-10-27 01:49:03 +0500

    Refactor app.js and add new chapters

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
-// Chapter 1
+// Chapter 1 -------------------------------------
 
 alert("Welcome to JavaScript World!");
 alert("Hello again! \n How’s your coding journey?");
 alert("JavaScript says: Let's make the web alive!");
 
-// Chapter 2
+// Chapter 2 -----------------------------------
 
 alert("My name is Daniyal!");
 alert("My Age Is 18!");
@@ -25,4 +25,63 @@ alert("Fun fact: I started coding before breakfast!");
 alert("I Have Started Coding!");
 
 alert("I was born to code — somewhere around 2007!");
+
 alert("Daniyal just ordered 2 pair of amazing Shoes from EcomStore!");
+
+// Chapter 4 -------------------------------------
+
+// legal variable
+let userName
+let adminName
+let myName
+let user123
+
+// Illegal variable
+let if
+let else
+let elseIf
+let class
+let this
+
+q no. 3
+b)sighs and letters and numbers
+c)sighs and letters and numbers
+d)case sensitive
+e)Js keyword
+
+// Chapter 5 ------------------------------
+// question 1
+let num1 = "1"
+let num2 = "2"
+let result = num1 + num2
+
+// question 2
+
+let num1 = "1"
+let num2 = "2"
+let result = num1 - num2
+
+let num1 = "1"
+let num2 = "2"
+let result = num1 * num2
+
+let num1 = "1"
+let num2 = "2"
+let result = num1 / num2
+
+let num1 = "1"
+let num2 = "2"
+let result = num1 % num2
+
+// question 4
+
+let ticketPrice = "600"
+let fiveTicket = ticketPrice * 5
+
+// question 5
+
+let randomNum = prompt("Enter Any Number", "5")
+let answer = 
+
+console.log(randomNum "*" 1 "=" answer)
+
